6606b53434b67f327e5a51e061cd7c61a2794a46
Rajlakhera321 <rajlakhera795@gmail.com>
2026-08-19 22:49:13 +0530

    new changes

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 3000;
 connection();
 app.use(express.json());
 
-console.log("Server is running on port 3000");
+console.log("Server is running on port 8000");
 app.use("/api/v1/user", require("./src/router/user"));
 app.use("/api/v1/book", require("./src/router/book"));
 
-console.log("new console for git check 1");
+console.log("new console for git check 234");
 app.listen(port, () => console.log(`Server is running on port ${port}`));
