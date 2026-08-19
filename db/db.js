dc1b69703ab2bb326b83afcd3eb827b829307774
Rajlakhera321 <rajlakhera795@gmail.com>
2026-08-19 22:52:12 +0530

    db changes done

--- db/db.js
+++ db/db.js
@@ -1,9 +1,11 @@
+
+
 const mongoose = require("mongoose");
 
 const connection = async () => {
     try {
         await mongoose.connect(process.env.DB_URL);
-        console.log("connected successfully");
+        console.log("connected successfully to the database mongodb atlas");
     } catch (error) {
         console.log(error);
     }
